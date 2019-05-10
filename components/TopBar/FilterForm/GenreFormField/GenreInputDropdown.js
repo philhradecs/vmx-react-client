945ed66368af2409e37359394dbf917602256b34
Philip Hradecsni <baumzeit@posteo.me>
2019-05-10 20:43:47 +0200

    add autosuggestion to genre form field

--- components/TopBar/FilterForm/GenreFormField/GenreInputDropdown.js
+++ components/TopBar/FilterForm/GenreFormField/GenreInputDropdown.js
@@ -1,32 +1,18 @@
 import { Box, FormField, TextInput } from 'grommet';
 import { useEffect, useLayoutEffect, useState } from 'react';
+import musicTypes from './discogsMusicTypes190510';
 
 function findAndSortMatches(items, keyword) {
   const re = new RegExp(keyword, 'i');
   const foundMatches = items.filter(({ label }) => label.match(re));
-  foundMatches.sort((a, b) => {
-    const aIndex = a.label.search(re);
-    const bIndex = b.label.search(re);
+  foundMatches.sort((a, b) => a.label > b.label);
 
-    if (aIndex === bIndex) {
-      return a.label > b.label;
-    }
-    return aIndex > bIndex;
-  });
   return foundMatches;
 }
 
-const musicTypes = [
-  { label: 'Punk Rock', value: 'style' },
-  { label: 'Papa Rock', value: 'style' },
-  { label: 'Rock', value: 'genre' },
-  { label: 'Chicago Jazz', value: 'style' },
-  { label: 'Chicago Rock', value: 'style' }
-];
-
-function GenreInputDropdown() {
+function GenreInputDropdown({ presetInput }) {
   const [matches, setMatches] = useState([]);
-  const [input, setInput] = useState(null);
+  const [input, setInput] = useState(presetInput);
 
   useEffect(() => {
     if (input && input.length > 1) {
@@ -41,13 +27,18 @@ function GenreInputDropdown() {
     <FormField name="musicType" label="Genre">
       <TextInput
         label="Genre"
+        placeholder="type here"
         dropAlign={{ top: 'bottom', left: 'left' }}
         dropContent={<Box pad="large" background="light-2" />}
         suggestions={matches}
-        onChange={event => setInput(event.target.value)}
+        value={input}
+        onChange={event => {
+          setInput(event.target.value);
+        }}
         onSelect={selectedOption => {
           setInput(selectedOption.suggestion.label);
         }}
+        onClick={event => event.target.select()}
         dropProps={{
           pad: { horizontal: '0.8rem', vertical: '0.5rem' },
           elevation: 'small'
