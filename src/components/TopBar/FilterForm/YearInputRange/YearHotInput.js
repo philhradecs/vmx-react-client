c9ece6412d27ceb75a42cca72e992bc5d36bf703
Philip Hradecsni <baumzeit@posteo.me>
2019-05-17 17:10:03 +0200

    refactor to use new reducer action

--- src/components/TopBar/FilterForm/YearInputRange/YearHotInput.js
+++ src/components/TopBar/FilterForm/YearInputRange/YearHotInput.js
@@ -17,17 +17,34 @@ export default forwardRef(function YearHotInput(
   }, [activeYears]);
 
   useEffect(() => {
-    const newYears = inputParser(input);
-
-    if (newYears && !newYears.every((year, i) => year === activeYears[i])) {
-      dispatch({ type: 'applyUserInput', payload: newYears });
-      updateFormValue(newYears);
+    if (input === '') {
+      dispatch({ type: 'clearField' });
+    } else {
+      const newYears = inputParser(input);
+      if (newYears && !newYears.every((year, i) => year === activeYears[i])) {
+        dispatch({ type: 'applyUserInput', payload: newYears });
+        dispatch({ type: 'openDrop' });
+        updateFormValue(newYears);
+      }
     }
   }, [input]);
 
   function handleInputFocus() {
-    dispatch({ type: 'readyInput' });
+    if (activeYears.length > 0) {
+      dispatch({ type: 'openDrop' });
+    }
   }
+
+  function handleKeyDown(event) {
+    if (event.keyCode === 38) {
+      dispatch({ type: 'incrementDropValues' });
+    }
+    if (event.keyCode === 40) {
+      dispatch({ type: 'decrementDropValues' });
+    }
+  }
+
+  // TODO: add scroll to inc & dec dropValues
 
   return (
     <TextInput
@@ -36,10 +53,12 @@ export default forwardRef(function YearHotInput(
       value={input}
       onChange={event => setInput(event.target.value)}
       onFocusCapture={handleInputFocus}
+      onKeyDown={handleKeyDown}
       plain
       onClick={event => event.target.select()}
       name={formProps.name}
       ref={ref}
+      placeholder={formProps.placeholder}
     />
   );
 });
