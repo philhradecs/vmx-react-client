06b8ac702fb2255d9d5e41a6eddc0526f50987c3
Philip Hradecsni <baumzeit@posteo.me>
2019-05-23 23:05:54 +0200

    improve serializer and parser logic

--- src/components/TopBar/FilterForm/YearInputRange/lib/parserSerializer.js
+++ src/components/TopBar/FilterForm/YearInputRange/lib/parserSerializer.js
@@ -1,33 +1,37 @@
-function parserSerializer(numRange, maxSpan) {
-  function serializer(arr = []) {
-    const list = arr.sort();
-    return list.length > 1 ? `${list[0]} - ${list[list.length - 1]}` : list[0];
+function serializer(arr = []) {
+  const list = arr.map(val => +val).sort();
+  if (list.length > 1) {
+    return `${list[0]} - ${list[list.length - 1]}`;
   }
-
-  function parser(input = '') {
-    const hasValue = input.toString().match(/\d{4}/g);
-    if (!hasValue) {
-      return false;
-    }
-
-    const extractedValues = hasValue.map(val => +val);
-
-    const yearArr =
-      extractedValues.length > 2
-        ? [extractedValues[0], extractedValues[1]]
-        : extractedValues;
-
-    const [min, max] = numRange;
-
-    const eachWithinRange = arr => arr.every(val => val >= min && val <= max);
-    const validRangeSpan = arr =>
-      Math.abs(arr[0] - arr[arr.length - 1]) <= maxSpan - 1;
-
-    return (
-      eachWithinRange(yearArr) && validRangeSpan(yearArr) && yearArr.sort()
-    );
+  if (list.length === 1) {
+    return list[0];
   }
-  return { parser, serializer };
+  return '';
 }
 
-export default parserSerializer;
+function parser(input = '', numRange, maxSpan) {
+  const hasValue = input.toString().match(/\d{4}/g);
+  if (!hasValue) {
+    return [];
+  }
+  const extractedValues = hasValue.map(val => +val);
+
+  let yearArr =
+    extractedValues.length > 2
+      ? [extractedValues[0], extractedValues[1]]
+      : extractedValues;
+
+  const [min, max] = numRange;
+
+  const eachWithinRange = arr => arr.every(val => val >= min && val <= max);
+  const validRangeSpan = arr =>
+    Math.abs(arr[0] - arr[arr.length - 1]) <= maxSpan - 1;
+  if (eachWithinRange(yearArr) && !validRangeSpan(yearArr)) {
+    const minYear = Math.min(...yearArr);
+    yearArr = [minYear, minYear + maxSpan - 1];
+  }
+
+  return eachWithinRange(yearArr) && validRangeSpan(yearArr) && yearArr.sort();
+}
+
+export { serializer, parser };
