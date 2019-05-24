5eeea95bb61938817c94b5d1c41cc1fa99ce3ca2
Philip Hradecsni <baumzeit@posteo.me>
2019-05-24 17:30:33 +0200

    avoid open drop on initial render

--- src/components/TopBar/FilterForm/InputAutosuggestion/InputAutosuggestion.js
+++ src/components/TopBar/FilterForm/InputAutosuggestion/InputAutosuggestion.js
@@ -1,5 +1,5 @@
-import { Box, TextInput } from 'grommet';
-import { useEffect, useRef, useState } from 'react';
+import { TextInput } from 'grommet';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function InputAutosuggestion({
   suggestionSort,
@@ -9,64 +9,65 @@ export default function InputAutosuggestion({
 }) {
   const [matches, setMatches] = useState([]);
   const [initialRender, setInitialRender] = useState(true);
+  const { value } = formProps;
 
-  function findAndSortMatches(keyword) {
-    const re = new RegExp(keyword, 'i');
-    let foundMatches = suggestionList.filter(({ label }) => label.match(re));
-    foundMatches.sort(suggestionSort);
+  const findAndSortMatches = useCallback(
+    keyword => {
+      const re = new RegExp(keyword, 'i');
+      let foundMatches = suggestionList.filter(({ label }) => label.match(re));
+      foundMatches.sort(suggestionSort);
 
-    if (suggestionUser) {
-      const isDuplicate =
-        foundMatches.length > 0
-          ? foundMatches.some(
-              ({ label }) => label.toLowerCase() === keyword.toLowerCase()
-            )
-          : false;
-      if (!isDuplicate) {
-        const userEntry = { label: keyword, value: 'style' };
-        foundMatches = [userEntry, ...foundMatches];
+      if (suggestionUser) {
+        const isDuplicate =
+          foundMatches.length > 0
+            ? foundMatches.some(
+                ({ label }) => label.toLowerCase() === keyword.toLowerCase()
+              )
+            : false;
+        if (!isDuplicate) {
+          const userEntry = { label: keyword, value: 'style' };
+          foundMatches = [userEntry, ...foundMatches];
+        }
       }
-    }
-
-    return foundMatches;
-  }
-
-  function sanitizeInput(input) {
-    return input
-      .split(' ')
-      .map(
-        word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
-      )
-      .join(' ');
-  }
+      return foundMatches;
+    },
+    [suggestionList, suggestionSort, suggestionUser]
+  );
 
   useEffect(() => {
-    const currentValue = formProps.value;
-    if (!initialRender && currentValue && currentValue.length > 1) {
-      const foundMatches = findAndSortMatches(currentValue);
+    if (!initialRender && value && value.length > 1) {
+      const foundMatches = findAndSortMatches(value);
       setMatches(foundMatches);
     } else {
       setMatches([]);
     }
-    setInitialRender(false);
-  }, [formProps.value]);
+  }, [findAndSortMatches, value, initialRender]);
 
   function handleSelect(event) {
+    function sanitizeInput(input) {
+      return input
+        .split(' ')
+        .map(
+          word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
+        )
+        .join(' ');
+    }
     const sanitizedInput = sanitizeInput(event.suggestion.label);
     formProps.onChange({ value: sanitizedInput });
   }
 
-  function handleClick(event) {
+  function handleFocusClick(event) {
     event.target.select();
+    setInitialRender(false);
   }
 
   return (
     <TextInput
-      name
       dropAlign={{ top: 'bottom', left: 'left' }}
       suggestions={matches}
       onSelect={handleSelect}
-      onClick={handleClick}
+      onClick={handleFocusClick}
+      onFocus={handleFocusClick}
       dropProps={{
         pad: { horizontal: '0.8rem', vertical: '0.5rem' },
         elevation: 'small'
