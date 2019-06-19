19b0810f6e23a7fce660ccf08a563bf44a146f11
Philip Hradecsni <baumzeit@posteo.me>
2019-06-19 20:10:58 +0200

    pass additional props from parent

--- src/components/TopBar/FilterForm/YearInputRange/YearInputRange.js
+++ src/components/TopBar/FilterForm/YearInputRange/YearInputRange.js
@@ -4,7 +4,7 @@ import InputRangeCompound from './InputRangeCompound';
 export default function YearInputRange({ onChange, ...props }) {
   return (
     <ContextProvider {...props}>
-      <InputRangeCompound onChange={onChange} />
+      <InputRangeCompound onChange={onChange} {...props} />
     </ContextProvider>
   );
 }
