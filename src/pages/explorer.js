8442d84f7fbb82bf6c1bc8a9b5e42b8912aa8a22
Philip Hradecsni <baumzeit@posteo.me>
2019-06-19 20:19:22 +0200

    add spinners and remove obsolete data combine function

--- src/pages/explorer.js
+++ src/pages/explorer.js
@@ -5,10 +5,10 @@ import { Box } from 'grommet';
 
 import TopBar from '../components/TopBar/TopBar';
 import CoverGrid from '../components/CoverGrid/CoverGrid';
+import LoadingGrid from '../components/CoverGrid/LoadingGrid';
 
 import musicTypes from '../components/TopBar/FilterForm/InputAutosuggestion/data/discogsMusicTypes190510';
-import { GET_SEARCH_RELEASES } from '../queries';
-import { combineSearchData } from '../utils/dataUtils';
+import { GET_SEARCH_RELEASES } from '../apollo/queries';
 
 function formatQueryForApollo(queryParam) {
   const query = { ...queryParam };
@@ -20,42 +20,35 @@ function formatQueryForApollo(queryParam) {
     query[matchedType] = query.musicType;
     delete query.musicType;
   }
-  if (typeof query.years === 'string') {
-    query.years = [query.years];
-  }
   return query;
 }
 
 const Explorer = withRouter(({ router }) => {
-  const apolloQuery = formatQueryForApollo(router.query);
   const [gridHeight, setGridHeight] = useState(null);
   const gridBox = useRef();
 
+  const apolloQuery = formatQueryForApollo(router.query);
+
   // FIXME: update height on top bar collapse
-  useLayoutEffect(() => {
-    setGridHeight(`${gridBox.current.clientHeight}px`);
-  }, []);
+  // useLayoutEffect(() => {
+  //   setGridHeight(`${gridBox.current.clientHeight}px`);
+  // }, []);
 
   return (
     <Box>
       <Box direction="column" height="100vh">
-        <TopBar prevQuery={router.query} />
+        <TopBar prevQuery={router.query} small />
         <Box overflow="auto" height="100%" ref={gridBox}>
           <Query query={GET_SEARCH_RELEASES} variables={apolloQuery}>
             {({ data, loading, error, fetchMore }) => {
-              if (loading) return 'Loading';
+              if (loading) return <LoadingGrid />;
               if (error) return `Error: ${error.message}`;
 
-              const combinedData = combineSearchData(data.searchReleases);
-
-              return combinedData.results.length === 0 ? (
+              const queryData = data.searchReleases;
+              return queryData.results.length === 0 ? (
                 'No Results'
               ) : (
-                <CoverGrid
-                  columns={5}
-                  data={combinedData}
-                  gridHeight={gridHeight}
-                />
+                <CoverGrid columns={5} data={queryData} />
               );
             }}
           </Query>
