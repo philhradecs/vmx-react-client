572bce334703ad280346134032fcdf2b7c2fb785
Philip Hradecsni <baumzeit@posteo.me>
2019-06-19 20:00:34 +0200

    add Keyboard event

--- src/components/TopBar/FilterForm/YearInputRange/InputRangeCompound.js
+++ src/components/TopBar/FilterForm/YearInputRange/InputRangeCompound.js
@@ -1,11 +1,13 @@
 import { useContext, useRef, useEffect } from 'react';
-import { StateContext } from './lib/contexts';
+import { Box, Keyboard } from 'grommet';
+import { StateContext, DispatchContext } from './lib/contexts';
 
 import YearSelectDrop from './YearSelectDrop';
 import YearHotInput from './YearHotInput';
 
-export default function InputRangeCompound({ onChange }) {
+export default function InputRangeCompound({ onChange, ...props }) {
   const { yearDropOpen, inputValue } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
 
   const firstRender = useRef(onChange);
   const inputRef = useRef();
@@ -13,10 +15,13 @@ export default function InputRangeCompound({ onChange }) {
   useEffect(() => {
     firstRender.current({ target: { value: inputValue } });
   }, [inputValue]);
+
   return (
-    <>
-      <YearHotInput ref={inputRef} />
-      {yearDropOpen && <YearSelectDrop inputRef={inputRef} />}
-    </>
+    <Keyboard onEsc={() => dispatch({ type: 'closeDrop' })} target="document">
+      <YearHotInput ref={inputRef} {...props} />
+      <Box fill="horizontal">
+        {yearDropOpen && <YearSelectDrop inputRef={inputRef} />}
+      </Box>
+    </Keyboard>
   );
 }
