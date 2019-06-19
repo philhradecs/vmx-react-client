07f9f0622bff5e1152ddd5aeb042c4ed1d2b61fa
Philip Hradecsni <baumzeit@posteo.me>
2019-06-19 20:10:01 +0200

    implement DetailsViewer logic

--- src/components/CoverGrid/CoverGrid.js
+++ src/components/CoverGrid/CoverGrid.js
@@ -1,16 +1,40 @@
-import { useMemo } from 'react';
-import { Box, Grid } from 'grommet';
+import { useState } from 'react';
+import { Grid, Layer, Keyboard } from 'grommet';
 
 import CoverGridTile from './CoverGridTile/CoverGridTile';
+import DetailsViewer from '../DetailsViewer/DetailsViewer';
 
 export default function CoverGrid({ data, columns }) {
   const { results } = data;
+  const [showDetailsViewer, setShowDetailsViewer] = useState(false);
+  const [detailsID, setDetailsID] = useState(null);
+
+  function openDetailsViewerAtID(id) {
+    setDetailsID(id);
+    setShowDetailsViewer(true);
+  }
 
   return (
-    <Grid columns={{ count: columns, size: 'auto' }}>
-      {results.map(release => (
-        <CoverGridTile key={release.id} data={release} />
-      ))}
-    </Grid>
+    <>
+      <Grid columns={{ count: columns, size: 'auto' }}>
+        {results.map(release => (
+          <CoverGridTile
+            key={release.id}
+            data={release}
+            openDetailsViewerAtID={openDetailsViewerAtID}
+          />
+        ))}
+      </Grid>
+      {showDetailsViewer && (
+        <Keyboard onEsc={() => setShowDetailsViewer(false)} target="document">
+          <Layer
+            position="center"
+            onClickOutside={() => setShowDetailsViewer(false)}
+          >
+            <DetailsViewer data={data.results} detailsID={detailsID} />
+          </Layer>
+        </Keyboard>
+      )}
+    </>
   );
 }
