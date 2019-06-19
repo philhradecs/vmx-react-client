86263a452211112bc0383e99ef08935d993faf99
Philip Hradecsni <baumzeit@posteo.me>
2019-06-19 20:16:35 +0200

    improve layout

--- src/components/TopBar/ControlBox/ControlBox.js
+++ src/components/TopBar/ControlBox/ControlBox.js
@@ -1,17 +1,18 @@
-import { Box } from 'grommet';
+import { Anchor, Box } from 'grommet';
+
 import FilterForm from '../FilterForm/FilterForm';
 import ViewSettings from '../ViewSettings/ViewSettings';
 
-function ControlBox({ isCollapsed, prevQuery }) {
+function ControlBox({ prevQuery, small }) {
   return (
-    <Box
-      height={isCollapsed ? 'xsmall' : 'small'}
-      direction="row"
-      fill="horizontal"
-      align="center"
-    >
-      <FilterForm isCollapsed prevQuery={prevQuery} />
-      {/* <ViewSettings isCollapsed /> */}
+    <Box direction="row" fill="horizontal" align="center">
+      <Box width="50px" justify="center" align="center">
+        <Anchor href="/">
+          <Box color="brand">VMX</Box>
+        </Anchor>
+      </Box>
+      <FilterForm prevQuery={prevQuery} small={small} />
+      {/* <ViewSettings isCollapsed small={small}/> */}
     </Box>
   );
 }
