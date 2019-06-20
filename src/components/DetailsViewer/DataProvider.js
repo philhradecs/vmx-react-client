91aa274e5759ec922d3bfac5b7d314828d1cd099
Philip Hradecsni <baumzeit@posteo.me>
2019-06-20 21:51:18 +0200

    add delayed loading of release details

--- src/components/DetailsViewer/DataProvider.js
+++ src/components/DetailsViewer/DataProvider.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useIdle from 'react-use/lib/useIdle';
 import { Query } from 'react-apollo';
 
 import { GET_RELEASE_DETAILS } from '../../apollo/queries';
 
 export default function DataProvider({ children, searchData, activeIndex }) {
-  return (
+  const isIdle = useIdle(200, false);
+
+  const props = {
+    detailsData: null,
+    searchData: searchData[activeIndex],
+    loading: null,
+    error: null
+  };
+
+  return isIdle ? (
     <Query
       query={GET_RELEASE_DETAILS}
       variables={{ id: searchData[activeIndex].id }}
     >
       {({ data, loading, error, fetchMore }) => {
-        const props = {
+        const queryProps = {
+          ...props,
           detailsData: data ? data.releaseDetails : null,
-          searchData: searchData[activeIndex],
           loading,
           error
         };
-        return React.cloneElement(children, props);
+        return React.cloneElement(children, queryProps);
       }}
     </Query>
+  ) : (
+    React.cloneElement(children, props)
   );
 }
