9f096305610c653d9877d7510575f325abc4a15e
Philip Hradecsni <baumzeit@posteo.me>
2019-06-28 23:06:20 +0200

    setup landing page overlay logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import { Box, Button, Layer } from 'grommet';
 import TopBar from '../components/TopBar/TopBar';
 
 function Home() {
-  return <TopBar />;
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  return (
+    <>
+      <TopBar />
+      {showOverlay && (
+        <Layer
+          // full
+          onEsc={() => setShowOverlay(false)}
+          onClickOutside={() => setShowOverlay(false)}
+        >
+          <Button label="close" onClick={() => setShowOverlay(false)} />
+        </Layer>
+      )}
+    </>
+  );
 }
 
 export default Home;
