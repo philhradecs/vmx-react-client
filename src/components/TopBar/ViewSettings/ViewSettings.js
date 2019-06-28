bb1653faa0b3f0088539412beb1abb33987c03a6
Philip Hradecsni <baumzeit@posteo.me>
2019-06-28 23:07:03 +0200

    add split view and tile size ui logic

--- src/components/TopBar/ViewSettings/ViewSettings.js
+++ src/components/TopBar/ViewSettings/ViewSettings.js
@@ -1,18 +1,76 @@
-import { useState } from 'react';
-import { Box, RadioButtonGroup, RangeInput } from 'grommet';
+import { useState, useEffect } from 'react';
+import { Box, Text } from 'grommet';
+import { Projects, Stop, FormAdd, FormSubtract, Split } from 'grommet-icons';
+
+import IconWrapper from '../../IconWrapper';
+
+const minColumns = 3;
+const maxColumns = 8;
 
 function ViewSettings() {
-  const [value, setValue] = useState('one');
-  return (
-    <Box direction="row" align="center" justify="center">
-      <RangeInput />
+  const [numColumns, setNumColumns] = useState(5);
+  const [splitView, setSplitView] = useState(false);
 
-      <RadioButtonGroup
-        name="doc"
-        options={['one', 'two']}
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
+  function decreaseNumColumns() {
+    const nextNum = numColumns - 1;
+    if (nextNum >= minColumns) {
+      setNumColumns(nextNum);
+    }
+  }
+
+  function increaseNumColumns() {
+    const nextNum = numColumns + 1;
+    if (nextNum <= maxColumns) {
+      setNumColumns(nextNum);
+    }
+  }
+
+  return (
+    <Box direction="row" align="center" justify="center" gap="2rem">
+      <Box gap="1.5rem" direction="row" align="center">
+        <Text>Size</Text>
+        <Box direction="row" gap="0.3rem">
+          <IconWrapper
+            onClick={decreaseNumColumns}
+            highlightColor="accent-4"
+            focusIndicator={false}
+            disabled={numColumns <= minColumns}
+          >
+            <FormSubtract color="neutral-3" />
+          </IconWrapper>
+          <Projects />
+          <IconWrapper
+            onClick={increaseNumColumns}
+            highlightColor="accent-4"
+            focusIndicator={false}
+            disabled={numColumns >= maxColumns}
+          >
+            <FormAdd color="neutral-3" />
+          </IconWrapper>
+        </Box>
+      </Box>
+      <Box gap="1.5rem" direction="row" align="center">
+        <Text>Layout</Text>
+        <Box direction="row" gap="0.3rem">
+          <IconWrapper
+            onClick={() => setSplitView(false)}
+            highlightColor="accent-4"
+            focusIndicator={false}
+            selected={!splitView}
+          >
+            <Stop size="1.8rem" color="neutral-3" />
+          </IconWrapper>
+          <IconWrapper
+            onClick={() => setSplitView(true)}
+            highlightColor="accent-4"
+            focusIndicator={false}
+            sticky
+            selected={splitView}
+          >
+            <Split color="neutral-3" />
+          </IconWrapper>
+        </Box>
+      </Box>
     </Box>
   );
 }
