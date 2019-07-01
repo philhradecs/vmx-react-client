062901f404af98d20773aa03aab2eae731f1bba4
Philip Hradecsni <baumzeit@posteo.me>
2019-07-01 16:29:10 +0200

    remove src folder

--- src/components/TopBar/FilterForm/YearInputRange/lib/contexts.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ConfigContext = React.createContext();
-export const DispatchContext = React.createContext();
-export const StateContext = React.createContext();
