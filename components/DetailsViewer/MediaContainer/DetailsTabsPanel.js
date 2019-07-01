5270806e41812a985e24ab0206c6d2df1e5ae8d9
Philip Hradecsni <baumzeit@posteo.me>
2019-07-01 21:02:24 +0200

    add DetailsTabsPanel

---
+++ components/DetailsViewer/MediaContainer/DetailsTabsPanel.js
@@ -0,0 +1,40 @@
+import { Box, Tabs, Tab, Text } from 'grommet';
+import { useContext, useState } from 'react';
+import { Disc, Group } from 'grommet-icons';
+import IconWrapper from '../../IconWrapper';
+import DataContext from '../DataProvider/context';
+
+export default function DetailsTabsPanel() {
+  const [activeIndex, setActivIndex] = useState(0);
+  const { detailsData, activeData } = useContext(DataContext);
+
+  const withWrapper = (icon, tabIndex) => (
+    <IconWrapper selected={activeIndex === tabIndex} highlightColor="accent-4">
+      {icon}
+    </IconWrapper>
+  );
+
+  return (
+    <Tabs activeIndex={activeIndex} onActive={tab => setActivIndex(tab)}>
+      <Tab title={withWrapper(<Disc color="brand" />, 0)}>
+        <Box>
+          {Object.keys(activeData)
+            .filter(
+              key => !['image', '__typename', 'title', 'id'].includes(key)
+            )
+            .map(key => (
+              <Box key={key} direction="row" justify="between" gap="1.5rem">
+                <Text weight="bold">
+                  {key[0].toUpperCase() + key.substr(1)}
+                </Text>
+                <Text>{JSON.stringify(activeData[key])}</Text>
+              </Box>
+            ))}
+        </Box>
+      </Tab>
+      <Tab title={withWrapper(<Group color="brand" />, 1)}>
+        <Box pad="medium">Artist description</Box>
+      </Tab>
+    </Tabs>
+  );
+}
