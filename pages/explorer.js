b23fd94adddb2a5038385174b5df1fafc4ffc130
Philip Hradecsni <baumzeit@posteo.me>
2019-07-16 18:44:55 +0200

    use data provider component, add pagination bar, add pagination props to format function

--- pages/explorer.js
+++ pages/explorer.js
@@ -1,16 +1,19 @@
 import { withRouter } from 'next/router';
-import { Query } from 'react-apollo';
 import { Box } from 'grommet';
 
 import TopBar from '../components/TopBar/TopBar';
 import CoverGrid from '../components/CoverGrid/CoverGrid';
-import LoadingGrid from '../components/CoverGrid/LoadingGrid';
 
 import musicTypes from '../data/discogsMusicTypes190510';
 import { GET_SEARCH_RELEASES } from '../apollo/queries';
+import ApolloDataProvider from '../components/ApolloDataProvider/ApolloDataProvider';
+import PaginationStatusBar from '../components/PaginationStatusBar/PaginationStatusBar';
 
 function formatQueryForApollo(queryParam) {
   const query = { ...queryParam };
+  query.page = parseInt(query.page, 10);
+  query.per_page = parseInt(query.per_page, 10);
+
   if (Object.prototype.hasOwnProperty.call(query, 'musicType')) {
     const matchedTypeEntry = musicTypes.find(
       ({ label }) => label === query.musicType
@@ -22,35 +25,29 @@ function formatQueryForApollo(queryParam) {
   return query;
 }
 
-const Explorer = withRouter(({ router }) => {
-  const apolloQuery = formatQueryForApollo(router.query);
+function Explorer({ router }) {
+  const apolloQuery = formatQueryForApollo(router.query); // translate 'musicType' form parameter to query parameter 'style' or 'genre'
+
+  const apolloOptions = {
+    variables: apolloQuery,
+    query: GET_SEARCH_RELEASES
+  };
 
   return (
-    <Box>
-      <Box direction="column" height="100vh">
-        <TopBar prevQuery={router.query} small />
-        <Box
-          overflow="auto"
-          height="100%"
-          pad={{ horizontal: 'medium', vertical: 'small' }}
-        >
-          <Query query={GET_SEARCH_RELEASES} variables={apolloQuery}>
-            {({ data, loading, error, fetchMore }) => {
-              if (loading) return <LoadingGrid />;
-              if (error) return `Error: ${error.message}`;
-
-              const queryData = data.searchReleases;
-              return queryData.results.length === 0 ? (
-                'No Results'
-              ) : (
-                <CoverGrid columns={5} data={queryData} />
-              );
-            }}
-          </Query>
+    <Box direction="column" height="100vh">
+      <TopBar prevQuery={router.query} small />
+      <ApolloDataProvider
+        apolloOptions={apolloOptions}
+        typeName="searchReleases"
+        load
+      >
+        <PaginationStatusBar />
+        <Box height="100%">
+          <CoverGrid columns={5} />
         </Box>
-      </Box>
+      </ApolloDataProvider>
     </Box>
   );
-});
+}
 
-export default Explorer;
+export default withRouter(Explorer);
