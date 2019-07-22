9a2f45e66a6c8710dcc7a14d8bb0581f5382b196
Philip Hradecsni <baumzeit@posteo.me>
2019-07-22 18:17:52 +0200

    refactor to show image as background

--- components/DetailsViewer/MediaContainer/MainImage.js
+++ components/DetailsViewer/MediaContainer/MainImage.js
@@ -3,21 +3,16 @@ import Ratio from 'react-ratio/lib/Ratio';
 
 export default function MainImage({ imageSrc }) {
   return (
-    <Box overflow="hidden" round="7px" fill>
+    <Box overflow="hidden" round="7px" fill pad="1rem">
       <Ratio>
-        <Box
-          margin="1rem"
-          overflow="hidden"
-          round="7px"
-          elevation="medium"
-          fill="horizontal"
-        >
-          <Image
-            src={imageSrc}
-            fallback="https://s3.amazonaws.com/detroitpubliclibrary/assets/images/material-cd.jpg"
-            fit="contain"
-            height="100%"
-            width="100%"
+        <Box overflow="hidden" round="7px" elevation="medium" fill>
+          <Box
+            fill
+            background={{
+              image: `url("${imageSrc}"), url('https://s3.amazonaws.com/detroitpubliclibrary/assets/images/material-cd.jpg')`,
+              position: 'center',
+              size: 'cover'
+            }}
           />
         </Box>
       </Ratio>
