2d57611121a2a38f44f5909326b0931fc02e8a8a
Philip Hradecsni <baumzeit@posteo.me>
2019-07-22 18:14:03 +0200

    use new ui wrapper, adjust to context property name change, implement memoization

--- components/CoverGrid/CoverGrid.js
+++ components/CoverGrid/CoverGrid.js
@@ -1,7 +1,5 @@
-import Router from 'next/router';
-
-import { useCallback, useState, useContext, useEffect } from 'react';
-import { Grid, Box } from 'grommet';
+import { useState, useContext, useEffect, useMemo } from 'react';
+import { Grid } from 'grommet';
 import InfiniteScroll from 'react-bidirectional-infinite-scroll';
 import styled from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
@@ -11,6 +9,7 @@ import DetailsViewerLayer from '../DetailsViewer/DetailsViewerLayer';
 import ApolloDataContext from '../ApolloDataProvider/context';
 import LoadingGridLayer from './LoadingGridLayer';
 import LoadingGrid from './LoadingGrid';
+import LoadMoreUIWrapper from './LoadMoreUIOverlay/LoadMoreUIWrapper';
 
 const TransitionBox = styled.div`
   &.tile-transition-enter,
@@ -33,96 +32,117 @@ const TransitionBox = styled.div`
 
 export default function CoverGrid({ columns }) {
   const {
-    data,
+    searchReleases,
     loading,
     error,
     hasMore,
     fetchMoreData,
     variables
   } = useContext(ApolloDataContext);
-  const [dataMemory, setDataMemory] = useState(null);
+
+  const defaultMemoryData = { results: [] };
+  const [dataMemory, setDataMemory] = useState(defaultMemoryData);
+  const [reachedTop, setReachedTop] = useState(false);
+  const [reachedBottom, setReachedBottom] = useState(false);
 
   useEffect(
     () => {
-      if (data && !loading) {
-        setDataMemory(data);
+      if (searchReleases && !loading) {
+        setDataMemory(searchReleases);
       }
     },
-    [data, loading]
+    [searchReleases, loading]
   );
 
   const [showDetailsViewer, setShowDetailsViewer] = useState(false);
   const [detailsID, setDetailsID] = useState(null);
 
-  const openDetailsViewerAtID = useCallback(id => {
-    setDetailsID(id);
-    setShowDetailsViewer(true);
-  }, []);
+  const { results } = loading ? dataMemory : searchReleases; // show previous grid data while loading new data
+
+  const coverTiles = useMemo(
+    () =>
+      results.map(release => {
+        const { id } = release;
+        const openDetailsViewer = event => {
+          event.stopPropagation();
+          setDetailsID(id);
+          setShowDetailsViewer(true);
+        };
+
+        return (
+          <CoverGridTile
+            key={id}
+            data={release}
+            openDetailsViewer={openDetailsViewer}
+          />
+        );
+      }),
+    [results]
+  );
 
   if (error) return error.message;
   if (loading && !dataMemory) return <LoadingGrid />;
 
-  const { results } = loading ? dataMemory : data; // show previous grid data while loading new data
-
-  const coverTiles = results.map(release => {
-    const { id } = release;
-    const openDetailsViewer = event => {
-      event.stopPropagation();
-      openDetailsViewerAtID(id);
-    };
-
-    return (
-      <CoverGridTile
-        key={id}
-        data={release}
-        openDetailsViewer={openDetailsViewer}
-      />
-    );
-  });
-
   function handleReachTop() {
     if (hasMore.prev) {
-      fetchMoreData.prevPage();
+      setReachedTop(true);
     }
   }
   function handleReachBottom() {
     if (hasMore.next) {
-      fetchMoreData.nextPage();
+      setReachedBottom(true);
     }
   }
 
+  function resetReachedEnds() {
+    setReachedBottom(false);
+    setReachedTop(false);
+  }
+
+  const trackScroll = { top: reachedTop, bottom: reachedBottom };
+
   return (
     <>
-      <InfiniteScroll
-        onReachBottom={handleReachBottom}
-        onReachTop={handleReachTop}
+      <LoadMoreUIWrapper
+        callbackTop={fetchMoreData.prevPage}
+        callbackBottom={fetchMoreData.nextPage}
+        trackScroll={trackScroll}
+        resetReachedEnds={resetReachedEnds}
+        minHeight={40}
+        limit={350}
+        step={40}
       >
-        <CSSTransition
-          in
-          appear
-          exit
-          timeout={500}
-          unmountOnExit
-          classNames="tile-transition"
+        <InfiniteScroll
+          onReachBottom={handleReachBottom}
+          onReachTop={handleReachTop}
         >
-          <TransitionBox>
-            <Grid
-              as="ul"
-              style={{ padding: '1rem', margin: 0 }}
-              columns={{ count: columns, size: 'auto' }}
-              gap="medium"
-            >
-              {coverTiles}
-            </Grid>
-          </TransitionBox>
-        </CSSTransition>
-      </InfiniteScroll>
+          <CSSTransition
+            in
+            appear
+            exit
+            timeout={500}
+            unmountOnExit
+            classNames="tile-transition"
+          >
+            <TransitionBox>
+              <Grid
+                as="ul"
+                style={{ padding: '1rem', margin: 0 }}
+                columns={{ count: columns, size: 'auto' }}
+                gap="medium"
+              >
+                {coverTiles}
+              </Grid>
+            </TransitionBox>
+          </CSSTransition>
+        </InfiniteScroll>
+      </LoadMoreUIWrapper>
 
-      {loading && <LoadingGridLayer text={`loading page ${variables.page}`} />}
+      {loading && <LoadingGridLayer text="loading" />}
 
       {showDetailsViewer && (
         <DetailsViewerLayer
-          searchData={data.results}
+          searchData={searchReleases.results}
           detailsID={detailsID}
           close={() => setShowDetailsViewer(false)}
         />
