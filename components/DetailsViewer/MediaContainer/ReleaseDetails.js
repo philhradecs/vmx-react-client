c63bf336dae3dd0f7e561f00a12e589b02ac89e8
Philip Hradecsni <ph@webundsoehne.com>
2019-10-28 01:08:15 +0100

    improve details view

--- components/DetailsViewer/MediaContainer/ReleaseDetails.js
+++ components/DetailsViewer/MediaContainer/ReleaseDetails.js
@@ -1,34 +1,19 @@
 import { useContext } from 'react';
-import { Box, Heading, Button } from 'grommet';
+import { useGlobal } from 'reactn';
+import { Box, Heading, Button, Text } from 'grommet';
 import useHover from 'react-use-hover';
 import { Filter } from 'grommet-icons';
 import ApolloDataContext from '../../ApolloDataProvider/context';
+import { ActiveSearchDataContext } from './context';
 
 export default function ReleaseDetails() {
-  const { releaseDetails, activeData } = useContext(ApolloDataContext);
+  const { releaseDetails } = useContext(ApolloDataContext);
+  const { activeData } = useContext(ActiveSearchDataContext);
 
-  // const { year, country, styles, genres } = releaseDetails;
+  const [updateFormField, setUpdateFormField] = useGlobal('updateFormField');
 
-  function DetailRow({ title, content, filterKey }) {
-    const data = Array.isArray(content) ? [...content] : [content];
-
-    return (
-      <Box direction="row" justify="between" gap="1.5rem" align="center">
-        <Heading level="2" size="1.1rem" margin="0">
-          {title}:
-        </Heading>
-        <Box direction="row" gap="0.5rem" margin="0.2rem" flex={false} wrap>
-          {data.map(item => <HoverItem value={item} filterKey={filterKey} />)}
-        </Box>
-      </Box>
-    );
-  }
-
-  function applyFilter(filterObj) {
-    console.log(filterObj);
-  }
-
-  function HoverItem({ value, filterKey }) {
+  // const { year, country, styles, genres, artists } = releaseDetails;
+  function FilterButton({ value, filterKey }) {
     const [isHovering, hoverProps] = useHover({
       mouseEnterDelayMS: 0,
       mouseLeaveDelayMS: 0
@@ -38,11 +23,10 @@ export default function ReleaseDetails() {
       borderRadius: '4px',
       padding: '0 0.25rem'
     };
-
     return (
       <Button
         label={value}
-        onClick={() => applyFilter({ [filterKey]: value })}
+        onClick={() => updateFormField(filterKey, value)}
         plain
         reverse
         icon={<Filter size="0.8rem" color={isHovering ? 'brand' : ''} />}
@@ -53,24 +37,86 @@ export default function ReleaseDetails() {
     );
   }
 
+  function DetailRow({ title, data, filterKey, dataKey }) {
+    const dataArray = Array.isArray(data) ? [...data] : [data];
+
+    return (
+      <Box direction="row" justify="between" gap="1.5rem" align="center">
+        <Heading level="2" size="1.1rem" margin="0">
+          {title}
+        </Heading>
+        <Box direction="row" gap="0.5rem" flex={false} wrap>
+          {dataArray.map(item => (
+            <FilterButton
+              value={dataKey ? item[dataKey] : item}
+              filterKey={filterKey}
+              key={`${filterKey}-${item}`}
+            />
+          ))}
+        </Box>
+      </Box>
+    );
+  }
+
+  function TrackList({ data, ...props }) {
+    return (
+      <Box
+        as="ul"
+        direction="column"
+        align="start"
+        fill="horizontal"
+        {...props}
+      >
+        {data.map(track => {
+          return (
+            <Box
+              direction="row"
+              justify="between"
+              gap="1.5rem"
+              key={track.position + track.title}
+              fill="horizontal"
+            >
+              <Box>{track.position}</Box>
+              <FilterButton value={track.title} filterKey="query" />
+              <Box>{track.duration}</Box>
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  }
+
   return (
     <Box dirction="column">
-      <DetailRow title="Year" content={activeData.year} filterKey="years" />
+      <DetailRow
+        title="Year"
+        data={activeData.year.toString()}
+        filterKey="years"
+      />
       <DetailRow
         title="Country"
-        content={activeData.country}
+        data={activeData.country}
         filterKey="country"
       />
       <DetailRow
         title="Styles"
-        content={activeData.styles}
+        data={activeData.styles}
         filterKey="musicType"
       />
       <DetailRow
         title="Genres"
-        content={activeData.genres}
+        data={activeData.genres}
         filterKey="musicType"
       />
+      <Box direction="row" justify="between">
+        <Heading level="2" size="1.1rem" margin="0">
+          Tracks
+        </Heading>
+        {!releaseDetails && <Text>loading ...</Text>}
+      </Box>
+      {releaseDetails && (
+        <TrackList data={releaseDetails.tracklist} animation="fadeIn" />
+      )}
     </Box>
   );
 }
